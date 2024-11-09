a09aaaebb6b8499931569ea69251feba9602c506
L3oN007 <89580650+L3oN007@users.noreply.github.com>
2024-11-09 23:04:48 +0700

    chore: format code

--- src/providers/react-query-provider.tsx
+++ src/providers/react-query-provider.tsx
@@ -1,37 +1,34 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
+import { useState } from "react";
 
-import {
-	QueryClient,
-	QueryClientProvider,
-} from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export const ReactQueryClientProvider = ({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) => {
-	const [queryClient] = useState(
-		() =>
-			new QueryClient({
-				defaultOptions: {
-					queries: {
-						// With SSR, we usually want to set some default staleTime
-						// above 0 to avoid refetching immediately on the client
-						staleTime: 60 * 1000,
-						refetchInterval: 60 * 1000,
-						retry: 3,
-						refetchOnWindowFocus: false,
-					},
-				},
-			})
-	);
-	return (
-		<QueryClientProvider client={queryClient}>
-			<ReactQueryDevtools initialIsOpen={false} />
-			{children}
-		</QueryClientProvider>
-	);
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // With SSR, we usually want to set some default staleTime
+            // above 0 to avoid refetching immediately on the client
+            staleTime: 60 * 1000,
+            refetchInterval: 60 * 1000,
+            retry: 3,
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  );
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      {children}
+    </QueryClientProvider>
+  );
 };
